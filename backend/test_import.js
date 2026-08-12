b854fb9412ad0c975c4c079eb9c5b87b939b454f
yogeshpatil0314@gmail.com <yogeshpatil0314@gmail.com>
2026-08-12 11:42:39 +0530

    git clean up

--- backend/test_import.js
+++
@@ -1,5 +0,0 @@
-import pdfParse from 'pdf-parse';
-console.log('pdfParse type:', typeof pdfParse);
-if (typeof pdfParse !== 'function') {
-    console.log('pdfParse keys:', Object.keys(pdfParse));
-}
--- backend/test_pdf2.js
+++
@@ -1,6 +0,0 @@
-import * as pdfPkg from 'pdf-parse';
-console.log('pdfPkg keys:', Object.keys(pdfPkg));
-const { PDFParse } = pdfPkg;
-if (PDFParse) {
-    console.log('PDFParse properties:', Object.getOwnPropertyNames(PDFParse.prototype));
-}
